6221abe74a69f0b8319e77944b1e637af4fd1395
KostasEreksonas <k.ereksonas@gmail.com>
2024-11-16 09:39:17 +0200

    Removed printing of current Fibonnaci number

--- benchmarks/fibonacci/fibonacci.py
+++ benchmarks/fibonacci/fibonacci.py
@@ -8,8 +8,6 @@ def fibonacci(n):
 
     for i in range(0, n):
         a, b = b, (a + b)
-        print(f"| [+] Counting {i+1}-nth number", end='\r', flush=True)
-    print(end='\n')
 
 # Driver Program
 fibonacci(int(sys.argv[1]))
